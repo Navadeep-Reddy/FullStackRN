524de71381fd9c5c0685b14ac7ced2c874e4d79a
Navadeep-Reddy <vnavadeepreddysatti@gmail.com>
2024-09-18 22:49:51 +0530

    Created a basic image with text and gsx styles

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,18 +1,36 @@
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Image } from 'react-native';
+import products from '../../assets/data/products';
+
+const product = products[0];
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
-      <Text style={{fontSize: 20, fontWeight: 'bold'}}>Pizza Pepperoni</Text>
-      <Text>$12.99</Text>
+      <Image source={{uri: product.image}} style={styles.image}/>
+      <Text style={styles.title}>{product.name}</Text>
+      <Text style={styles.price}>${product.price}</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container : {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'gray',
+    padding: 10,
+    borderRadius: 20,
   },
+  title : {
+    fontWeight: '600',
+    fontSize: 18,
+    marginVertical: 10,
+    
+  },
+  price : {
+    fontWeight: 'bold',
+    color: '#0a7ea4',
+  },
+  image: {
+    width: '100%',
+    aspectRatio: 1,
+  }
 });
